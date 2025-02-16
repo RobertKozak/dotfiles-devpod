a9603448a869309c235bcbed181a2bb065bf4754
Robert Kozak <rkozak@gmail.com>
2025-02-16 06:56:48 -0800

    UPdate install

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ ln -sf "$PWD/tmux" "$XDG_CONFIG_HOME/tmux/"
 ln -sf "$PWD/direnv" "$XDG_CONFIG_HOME/direnv/"
 
 ln -sf "$PWD/.*" "$HOME/"
+ln -sf "$PWD/starship.toml" "$XDG_CONFIG_HOME/startship.toml"
+
+curl -sS https://starship.rs/install.sh | sh
