86b00f768db4852f1854bd23bce5bd690b38ebe4
Robert Kozak <rkozak@gmail.com>
2025-02-16 06:44:25 -0800

    update install

--- install.sh
+++ install.sh
@@ -2,11 +2,3 @@
 
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
-
-# Create symlinks for existing configurations
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/tmux" "$XDG_CONFIG_HOME"/tmux
-ln -sf "$PWD/direnv" "$XDG_CONFIG_HOME"/direnv
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
-
-cp .* "$HOME/"
